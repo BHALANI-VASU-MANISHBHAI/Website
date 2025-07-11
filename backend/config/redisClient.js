c0c62cb60eb0132da5eb9b7773ff7834d53acde8
BHALANI-VASU-MANISHBHAI <u23cs106@coed.svnit.ac.in>
2025-07-12 00:30:17 +0530

    redis issue

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -2,6 +2,10 @@ import { createClient } from "redis";
 
 const redisClient = createClient({
   url: process.env.REDIS_URL || "redis://localhost:6379",
+  socket: {
+    tls: process.env.REDIS_URL?.includes("rediss://") || true, // enable TLS
+    rejectUnauthorized: false, // allow self-signed certs (Redis Cloud needs this)
+  },
 });
 
 redisClient.on("connect", () => {
