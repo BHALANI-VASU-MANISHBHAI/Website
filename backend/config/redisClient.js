8828ff214e6859a64c48545318bd033672110cfe
BHALANI-VASU-MANISHBHAI <u23cs106@coed.svnit.ac.in>
2025-07-12 00:47:17 +0530

    DONE

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -1,27 +1,16 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  username: "default",
+  password: "EfOO0c34fFw3PyBii0g4WJCR8ALxy2w1",
   socket: {
-    tls: process.env.REDIS_URL?.includes("rediss://") || true, // enable TLS
-    rejectUnauthorized: false, // allow self-signed certs (Redis Cloud needs this)
+    host: "redis-12872.c263.us-east-1-2.ec2.redns.redis-cloud.com",
+    port: 12872,
   },
 });
 
-redisClient.on("connect", () => {
-  console.log("✅ Redis connected successfully");
-});
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
-redisClient.on("error", (err) => {
-  console.error("❌ Redis Client Error", err);
-});
-
-(async () => {
-  try {
-    await redisClient.connect();
-  } catch (err) {
-    console.error("❌ Redis connection failed:", err);
-  }
-})();
+await redisClient.connect();
 
 export { redisClient };
